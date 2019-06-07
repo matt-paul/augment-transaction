23018e83d1d90d9113f0e1e40a45ab5f22e3fd2e
Matt Paul <mattpaul001@icloud.com>
2019-06-07 17:31:04 +0100

    Add types, adjust naming

--- index.ts
+++ index.ts
@@ -1,15 +1,20 @@
-const getEnviroCertificate = (retailer: string) => {
-  const fancyDb = [
-    {
-      name: "Sainsburys",
-      rating: "Awesome"
-    },
-    {
-      name: "PayPal",
-      rating: "Not good"
-    }
-  ]
+interface DatabaseEntry {
+  name: string
+  rating: string
+}
 
+export const fancyDb: Array<DatabaseEntry> = [
+  {
+    name: "Sainsburys",
+    rating: "Awesome"
+  },
+  {
+    name: "PayPal",
+    rating: "Not good"
+  }
+]
+
+const addNote = (retailer: string) => {
   const result = fancyDb.find(entry => entry.name === retailer)
   if (result) {
     return `${result.name} is rated ${result.rating} by Carbon Trust`
@@ -17,24 +22,33 @@ const getEnviroCertificate = (retailer: string) => {
   return `This retailer has no data yet`
 }
 
-export const augment = (body: any) => {
+interface Data {
+  description: string
+}
+interface Body {
+  type: string
+  data: Data
+}
+
+export const augment = (body: Body) => {
   const data = {
     ...body.data,
-    note: getEnviroCertificate(body.data.description)
+    note: addNote(body.data.description)
   }
   return { ...body, data }
 }
 
 export const handler = async (event: any) => {
-  console.log(event)
   const body = JSON.parse(event.body)
 
   // const trransactionID = body.data.id
   // const augmentTransaction = JSON.stringify(augment(body))
   // post(augmentedTransation)
+
+  const answer = augment(body)
   return {
     statusCode: "200",
-    body: JSON.stringify(augment(body)),
+    body: JSON.stringify(answer),
     headers: {
       "Content-Type": "application/json"
     }
