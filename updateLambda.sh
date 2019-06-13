d80ef2c6a11d57f3027310f9e35ca3614f4595f5
Matt Paul <mattpaul001@icloud.com>
2019-06-13 10:01:43 +0100

    Add comment in bash script

--- updateLambda.sh
+++ updateLambda.sh
@@ -8,4 +8,5 @@ tsc
 # Create zip file
 cd dist && zip -r ../infrastructure/augmentLambda.zip . ../node_modules
 
+# Update Lambda funciton with the newly created Zip file from above
 aws lambda update-function-code --function-name augment-transaction --zip-file fileb://../infrastructure/augmentLambda.zip
